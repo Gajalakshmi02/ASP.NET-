4aa7a06d6a8825fd83e7c6042e74f5cf62935e0c
Gajalakshmi02 <164175599+Gajalakshmi02@users.noreply.github.com>
2024-03-21 12:47:49 +0530

    Add files via upload

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,37 @@
+﻿using EmployeeSalary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EmployeeSalary.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index()
+        {
+            return View(new Salary());
+        }
+        [HttpPost]
+        public ActionResult Index(Salary s)
+        {
+            if(s.sal > 50000)
+            {
+                s.tax = s.sal * 10 / 100;
+            }
+            else if (s.sal >30000)
+            {
+                s.tax = s.sal * 5 / 100;
+            }
+            else
+            {
+                s.tax = 0;
+            }
+            s.nsal = s.sal - s.tax;
+
+            return View(s);
+        }
+    }
+}
---
+++ Models/Salary.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EmployeeSalary.Models
+{
+    public class Salary
+    {
+        public string name { get; set; }
+        public float sal { get; set; }
+        public float tax { get; set; }
+        public float nsal { get; set; }
+    }
+}
